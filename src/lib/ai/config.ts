0fed645a78851d1087e79035413a3bed226be341
Kevin Tam <kevin@glorat.net>
2023-06-04 20:56:25 +0800

    fix: no need for key on boot

--- src/lib/ai/config.ts
+++ src/lib/ai/config.ts
@@ -1,6 +1,4 @@
 import {Configuration, OpenAIApi} from 'openai';
-// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-// dotenv.config()
 
 type OpenAIEngine = 'text-ada-001' |  'text-davinci-003'
 
@@ -11,8 +9,10 @@ export const Config = {
   embedModel: 'text-embedding-ada-002'
 }
 
-if (!Config.openAiApiKey) throw new Error('OPENAPI_KEY env not setup. Please check .env file or environment variables')
+export const getOpenAIAPI = () => {
+  if (!Config.openAiApiKey) throw new Error('OPENAPI_KEY env not setup. Please check .env file or environment variables')
 
-export const getOpenAIAPI = () => new OpenAIApi(new Configuration({
-  apiKey: Config.openAiApiKey,
-}));
+  new OpenAIApi(new Configuration({
+    apiKey: Config.openAiApiKey,
+  }));
+}
