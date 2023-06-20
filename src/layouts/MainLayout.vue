d922c8a49598b482b6f1d7bc50082c4ac8c42e04
Kevin Tam <kevin.tam@olea.net>
2023-06-20 10:09:50 +0800

    feat: support custom openai endpoint such as Azure

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,6 +15,7 @@
           GPT Apps
         </q-toolbar-title>
 
+        <div><q-input label="Open AI Endpoint" type="text" filled v-model="apiUrl" @update:model-value="onApiUrlChange"></q-input></div>
         <div><q-input label="Open AI Key" type="password" filled v-model="openaikey" @update:model-value="onKeyChange"></q-input></div>
       </q-toolbar>
     </q-header>
@@ -36,23 +37,33 @@
 <script setup lang="ts">
 import {onMounted, ref} from 'vue'
 import MyAside from 'layouts/MyAside.vue'
-import {Config} from 'src/lib/ai/config'
+import {Config, OpenAIParams} from 'src/lib/ai/config'
 
 const leftDrawerOpen = ref(false)
 
 const openaikey = ref('')
+const apiUrl = ref('')
+
 onMounted(() => {
   const key = localStorage?.getItem('openaikey')
   if (key) {
     openaikey.value = key
-    Config.openAiApiKey = key
+    OpenAIParams.apiKey = key
+  }
+  const url = localStorage?.getItem('apiUrl')
+  if (url) {
+    apiUrl.value = url
+    OpenAIParams.basePath = url
   }
 })
 function onKeyChange() {
   localStorage?.setItem('openaikey', openaikey.value)
-  Config.openAiApiKey = openaikey.value
+  OpenAIParams.apiKey = openaikey.value
 }
-
+function onApiUrlChange() {
+  localStorage?.setItem('apiUrl', apiUrl.value)
+  OpenAIParams.basePath = apiUrl.value
+}
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
