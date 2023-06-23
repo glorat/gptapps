2b3708c1da26e1137cd83dfe5a0d5bd22482a170
Kevin Tam <kevin@glorat.net>
2023-06-23 21:56:51 +0800

    refactor: apply gpt recommended improvements

--- src/components/AudioTranscriber.vue
+++ src/components/AudioTranscriber.vue
@@ -8,13 +8,14 @@
 </template>
 
 <script setup lang="ts">
-import {Ref, ref} from 'vue'
-import {createTranscription} from 'src/lib/ai/openaiFacade'
+import { Ref, ref } from 'vue'
+import { createTranscription } from 'src/lib/ai/openaiFacade'
 
-const recording = ref(false);
-const mediaRecorder:Ref<MediaRecorder|null> = ref(null);
-const audioUrl:Ref<string> = ref('');
+const recording: Ref<boolean> = ref(false);
+const mediaRecorder: Ref<MediaRecorder | null> = ref(null);
+const audioUrl: Ref<string> = ref('');
 const emit = defineEmits(['message'])
+
 const toggleRecording = async () => {
   try {
     if (recording.value && mediaRecorder.value) {
@@ -26,7 +27,7 @@ const toggleRecording = async () => {
       recording.value = true;
       audioUrl.value = '';
 
-      const recordedChunks:Blob[] = [];
+      const recordedChunks: Blob[] = [];
       mediaRecorder.value.ondataavailable = (event) => {
         if (event.data.size > 0) {
           recordedChunks.push(event.data);
@@ -34,12 +35,17 @@ const toggleRecording = async () => {
       };
 
       mediaRecorder.value.onstop = async () => {
-        const blob = new Blob(recordedChunks, { type: 'audio/wav' });
-        const file = new File([blob], 'recording.wav', { type: 'audio/wav' });
-        audioUrl.value = URL.createObjectURL(file);
-        const text = await createTranscription({blob})
-        emit('message', text);
-        mediaRecorder.value = null
+        try {
+          const blob = new Blob(recordedChunks, { type: 'audio/wav' });
+          const file = new File([blob], 'recording.wav', { type: 'audio/wav' });
+          audioUrl.value = URL.createObjectURL(file);
+          const text = await createTranscription({ blob });
+          emit('message', text);
+        } catch (error) {
+          console.error('Error during transcription:', error);
+        } finally {
+          mediaRecorder.value = null;
+        }
       };
 
       mediaRecorder.value.start();
@@ -53,6 +59,9 @@ const playRecording = () => {
   if (audioUrl.value) {
     const audioElement = new Audio(audioUrl.value);
     audioElement.play();
+    audioElement.onended = () => {
+      audioUrl.value = '';
+    };
   }
 };
 </script>
