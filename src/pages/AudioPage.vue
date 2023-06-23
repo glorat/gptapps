2198e455a3bf90dff60eb5ef7c0871adf9abdce1
Kevin Tam <kevin@glorat.net>
2023-06-23 21:40:14 +0800

    feat: audio page multilingual support

--- src/pages/AudioPage.vue
+++ src/pages/AudioPage.vue
@@ -5,20 +5,49 @@
 </template>
 
 <script setup>
-import AudioTranscriber from "components/AudioTranscriber.vue";
-import {answerMe} from "src/lib/ai/openaiFacade";
+import AudioTranscriber from 'components/AudioTranscriber.vue';
+import {answerMe} from 'src/lib/ai/openaiFacade';
 
 async function onRecordedMessage(msg) {
   console.log(msg);
-  const res = await answerMe({context: 'You are a helpful AI assistant giving concise responses in one or two sentences', userPrompt: msg, initPrompt: ''})
+  const res = await answerMe({context: 'You are a helpful AI assistant giving concise responses in one or two sentences. Only use a single language in response.', userPrompt: msg, initPrompt: ''})
   console.log(res)
   speakText(res);
 }
 
+import {franc} from 'franc';
+
+const francToBCP47 = {
+  cmn: 'zh-CN',
+  eng: 'en',
+  sco: 'en',
+  spa: 'es',
+  deu: 'de',
+  fra: 'fr',
+  jpn: 'ja',
+  ita: 'it',
+  rus: 'ru',
+  kor: 'ko',
+};
+
 function speakText(text) {
-  const utterance = new SpeechSynthesisUtterance(text);
-  speechSynthesis.speak(utterance);
+  const options = {
+    only: Object.keys(francToBCP47),
+  };
+
+  const detectedLanguage = franc(text, options);
+  const langCode = francToBCP47[detectedLanguage];
+
+  if (langCode) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = langCode;
+    speechSynthesis.speak(utterance);
+  } else {
+    console.error('Unsupported language:', detectedLanguage);
+  }
 }
+
+
 
 </script>
 
